36982c9f4e721a8e7acacde8ec7ccfe9a00c1909
danixy8 <danielguve2@gmail.com>
2022-09-03 17:04:57 -0400

    se agrego oauth de google

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,8 @@
-const { response } = require('express');
+const { response, json } = require('express');
 const bcryptjs = require('bcryptjs');
-const Usuario = require('../models/user');
+const User = require('../models/user');
 const { generateJWT } = require('../helpers/generate-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req, res = response) => {
 
@@ -10,7 +11,7 @@ const login = async(req, res = response) => {
   try {
 
     //verificar email existe
-    const user = await Usuario.findOne({ email });
+    const user = await User.findOne({ email });
     if (!user){
       return res.status(400).json({
         msg: 'incorrect user/password'
@@ -48,6 +49,54 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSignIn = async (req, res) => {
+
+  const {id_token} = req.body;
+
+  try {
+    const {name, img, email} = await googleVerify( id_token )
+
+    let user = await User.findOne({email});
+
+    if(!user){
+      //si no tiene hacemos que cree un usuario
+      const data = {
+        name,
+        email,
+        role: "USER_ROLE",
+        password: ':P',
+        img,
+        google: true
+      }
+
+      user = new User(data);
+      await user.save();
+    }
+
+    //si el usuario en DB esta deshabilitado/bloqueado
+    if(!user.state){
+      return res.status(401).json({
+        msg: 'contact admin, user blocked'
+      })
+    }
+
+    //si el correo de google existe en BD creamos un token
+    const token = await generateJWT(user.id);
+
+    res.json({
+      user,
+      token
+    })
+
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: 'El token no se pudo verificar'
+    })
+  }
+}
+
 module.exports = {
-  login
+  login,
+  googleSignIn
 }
